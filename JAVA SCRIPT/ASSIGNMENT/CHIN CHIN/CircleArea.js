27ce3baf852891174157beca641eba848dd08226
BIGDUKE004 <elijahmiracle2007@gmail.com>
2026-05-08 08:42:40 +0100

    EASTER EGGS ASSIGHNMENT

--- JAVA SCRIPT/ASSIGNMENT/CHIN CHIN/CircleArea.js
+++
@@ -1,9 +0,0 @@
-const input = require("prompt-sync")();
-
-const radius = Number(input("Enter  radius of cylinder: "));
-
-const height = Number(input("Enter height of cylinder: "))
-
-let area = ( 2 * (Math.PI) * radius ) * (radius + height);
-
-console.log(area.toFixed(2));
--- JAVA SCRIPT/ASSIGNMENT/CHIN CHIN/Positive.js
+++
@@ -1,6 +0,0 @@
-let numberOne = 9;
-let numberTwo = 15;
-
-let difference = Math.abs(numberOne - numberTwo);
-
-console.log(`The difference = ${difference}`)
